e7154777485411f9647cc0a7740c51581cb171cb
Gabriel Espínola <gabrielsespinola@gmail.com>
2026-03-24 09:50:23 -0300

    Add files via upload

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,55 @@
+import { FaUser, FaLock } from "react-icons/fa"
+
+import { useState } from "react";
+
+import "./Login.css";
+
+const Login = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+
+    alert("enviando os dados:" + username + " - " + password);
+
+
+};
+
+  return (
+    <div className="container">
+        
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o sistema</h1>
+            <div className="input-field">
+                <input type="email" placeholder='E-mail' onChange={(e)=> setUsername(e.target.value)} />
+                <FaUser className="icon"/>
+            </div>
+            <div className="input-field">
+                <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value) } />
+                <FaLock className="icon"/>
+            </div>
+
+            <div className="recall-forget">
+                <label>
+                    <input type="checkbox" />
+                    Lembre de mim
+                    
+                </label>
+                <a href="http://">Esqueci minha senha</a>
+            </div>
+            <button>Entrar</button>
+
+            <div className="singup-link">
+                <p>
+                    Não tem uma conta? <a href="http://">Cadastre-se</a>
+                </p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
